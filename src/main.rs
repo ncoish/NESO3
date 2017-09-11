0a5d58241159b7b9b3a89e6fbeef9b3c9dd12f0e
Nick Coish <nick.coish@gmail.com>
2017-09-11 01:59:23 -0400

    Add folder structure and begin basic ROM parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod rom_parser;
+mod ppu;
+
 fn main() {
     println!("Hello, world!");
+    let filepath = std::path::Path::new("C:/Users/megab/Downloads/nestest.nes");
+    let rom = rom_parser::parser::parse_rom(filepath.to_str().unwrap());
+    println!("Rom data: {:?}", rom);
 }
