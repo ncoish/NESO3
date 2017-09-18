70ec97a6d87dc3ec8f603f860c7d995bd29a4d91
Nick Coish <nick.coish@gmail.com>
2017-09-17 22:50:09 -0400

    Add basic command line interface for testing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,39 @@
+extern crate clap;
+
+use std::io;
+use clap::{Arg, App};
+
 mod parser;
 mod ppu;
 mod memory;
 
 fn main() {
-    println!("Hello, world!");
-    let filepath = std::path::Path::new("../test/test_roms/nestest.nes");
-    let rom = parser::parse_rom(filepath.to_str().unwrap());
-    println!("Rom data: {:?}", rom);
+    let matches = App::new("NESO3")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about("NES emulator written in Rust")
+        .arg(Arg::with_name("test")
+            .short("t")
+            .long("test")
+            .help("Enter test mode to test various ROMs"))
+        .get_matches();
+
+    if matches.is_present("test") {
+        let stdin = io::stdin();
+        println!("Entering test mode...");
+        loop {
+            let mut input = String::new();
+            println!("Enter ROM filepath:");
+            stdin.read_line(&mut input).unwrap();
+            let filepath = std::path::Path::new(input.trim());
+            let rom = match parser::parse_rom(filepath.to_str().unwrap()) {
+                Ok(a) => a,
+                Err(e) => {
+                    println!("Error: {:?}", e);
+                    continue
+                }
+            };
+            println!("Rom data: {:?}", rom);
+        }
+    }
 }
